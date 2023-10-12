afc9b2b0efbb487e94ac1118577094aea65eb572
prem <thechilldeveloper@gmail.com>
2023-10-12 12:10:52 +0530

    commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,80 @@
+/*
+let js = 'amazing';
+// console.log(40+8+23-10);
+
+console.log("jonas");
+let firstName = "Bob";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+//assignment 1 
+let country = 'india';
+let continent = 'asia';
+let population = '121M';
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'true')
+javascriptIsFun = 'yes!'
+console.log(typeof javascriptIsFun)
+
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+
+
+
+const now = 2023
+const agePrem = now - 2001;
+const ageTaeju = now - 2000;
+console.log(agePrem, ageTaeju)
+
+console.log(agePrem *2, agePrem/10 , 2 ** 3);
+// 2**3 means 2 to the power of 3 ie. 2*2*2 
+
+const firstName = 'prem'
+const lastName = 'sathvik'
+
+console.log(firstName + ' '+ lastName);
+
+// Assignment operators
+
+let x = 10 + 5; // x = 15
+x += 10;
+x *= 2;
+x ++
+x --
+x-- 
+console.log (x);
+
+// comparison operators
+
+console.log(agePrem > ageTaeju);
+console.log(agePrem >= 18);
+
+
+const adult = agePrem >= 18;
+
+
+
+const now = 2023
+const agePrem = now - 2001
+const ageTaeju = now - 2000
+
+console.log(now-2001 > now-2000);
+
+const averageAge = (agePrem + ageTaeju)/2
+console.log(agePrem, ageTaeju, averageAge)
+
+*/
